f9b81f6c7bd38eb2c018a0432e466354550cb4df
Brandon Alvarez <nitroflare10@outlook.com>
2023-09-20 21:54:37 -0600

    player movement shell

--- player/Player.cs
+++ player/Player.cs
@@ -4,7 +4,7 @@ using System;
 public partial class Player : CharacterBody2D
 {
 
-	[Export] private float player_speed;
+	[Export] private float Player_Speed { get; set; } = 20f;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -16,7 +16,7 @@ public partial class Player : CharacterBody2D
 	{
 		float move_x = Input.GetAxis("player_move_left", "player_move_right");
 		float move_y = Input.GetAxis("player_move_up", "player_move_down");
-		Velocity = new Vector2(move_x, move_y).Normalized() * player_speed;
+		Velocity = new Vector2(move_x, move_y).Normalized() * Player_Speed * (float)delta;
 		if (Mathf.Abs(move_x) > 0 || Mathf.Abs(move_y) > 0)
 			GD.Print(Velocity);
 		MoveAndSlide();
