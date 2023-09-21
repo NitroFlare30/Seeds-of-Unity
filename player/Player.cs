01aa84b0c5d71aeadf59dbbaef46b83330b7ab74
Brandon Alvarez <nitroflare10@outlook.com>
2023-09-20 23:59:28 -0600

    player animations and camera follow

--- player/Player.cs
+++ player/Player.cs
@@ -4,11 +4,17 @@ using System;
 public partial class Player : CharacterBody2D
 {
 
-	[Export] private float Player_Speed { get; set; } = 20f;
+	[Export] private float Player_Speed { get; set; } = 50f;
+
+	private AnimationPlayer _animationPlayer;
+
+	private int lastXPos;
+	private int lastYPos;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,9 +22,44 @@ public partial class Player : CharacterBody2D
 	{
 		float move_x = Input.GetAxis("player_move_left", "player_move_right");
 		float move_y = Input.GetAxis("player_move_up", "player_move_down");
-		Velocity = new Vector2(move_x, move_y).Normalized() * Player_Speed * (float)delta;
-		if (Mathf.Abs(move_x) > 0 || Mathf.Abs(move_y) > 0)
-			GD.Print(Velocity);
+		Velocity = new Vector2(move_x, move_y).Normalized() * Player_Speed;
+		if (move_x > 0)
+		{
+			_animationPlayer.Play("Walk_Right");
+			lastXPos = 1;
+			lastYPos = 0;
+		}
+		else if (move_x < 0)
+		{
+			_animationPlayer.Play("Walk_Left");
+			lastXPos = -1;
+			lastYPos = 0;
+		}
+		else if (move_y < 0)
+		{
+			_animationPlayer.Play("Walk_Up");
+			lastXPos = 0;
+			lastYPos = -1;
+		}
+		else if (move_y > 0)
+		{
+			_animationPlayer.Play("Walk_Down");
+			lastXPos = 0;
+			lastYPos = 1;
+		}
+		else
+		{
+			if (lastXPos > 0)
+				_animationPlayer.Play("Idle_Right");
+			else if (lastXPos < 0)
+				_animationPlayer.Play("Idle_Left");
+			else if (lastYPos > 0)
+				_animationPlayer.Play("Idle_Down");
+			else if (lastYPos < 0)
+				_animationPlayer.Play("Idle_Up");
+			lastXPos = 0;
+			lastYPos = 0;
+		}
 		MoveAndSlide();
 		// Register player sprite direction based on movement
 		
